1f926165e47e1dbf75cce24f2060d8ea30d8c8db
STANELY <STANELY@DESKTOP-GFPAADS>
2025-03-04 18:08:13 +0530

    Add project files.

---
+++ WebApplication2/Controllers/CatorgoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.database;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CatorgoryController : Controller
+    {
+        private readonly DbClass _db;
+
+
+        public CatorgoryController(DbClass db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Catorgory> objCategoryList = _db.Catorgory.ToList();        
+                return View();
+        }
+    }
+}
---
+++ WebApplication2/database/DbClass.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.Xml;
+using WebApplication2.Models;
+
+namespace WebApplication2.database
+{
+    public class DbClass :DbContext
+    {
+        public DbClass(DbContextOptions<DbClass>options):base(options)
+        {
+    }
+        public DbSet<Catorgory> Catorgory { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Catorgory>().HasData(
+                new Catorgory { Id = 1, Name = "Stanely", DisplayOrder = 1 },
+                 new Catorgory { Id = 2, Name = "nf", DisplayOrder = 2 },
+                 new Catorgory { Id = 3, Name = "67", DisplayOrder = 3 }
+                );
+        }
+    }
+}
